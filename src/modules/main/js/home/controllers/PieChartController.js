5ec8565a16266fae8ca86d5b93bb35be56cb7d1c
Christophe Marchadour <christophe.marchadour@gmail.com>
2015-03-21 23:11:13 +0100

    Introduce Pie Chart demo controller

---
+++ src/modules/main/js/home/controllers/PieChartController.js
@@ -0,0 +1,39 @@
+define([
+	'angular',
+	'lodash',
+	'main/home/HomeApp'
+],function(angular,_,HomeApp){
+
+	HomeApp.controller('PieChartController',['$scope','$timeout',function($scope,$timeout){
+		$scope.config0 = {};
+
+		$scope.config1 = {
+			colors : ['#4864AF','#7891D5','#E3EAFC','#DEDEDE','#EFEFEF']
+		};
+
+		$scope.data0 = [400,200,150,180,200];
+		$scope.data1 = [124,4987,1900];
+
+		$scope.miniPieConfiguration = {
+			radius : 20,
+			strokeWidth : 5
+		}		
+
+		$scope.generateRandomArray = function(size){
+			var array = []
+			for(var i = 0; i < size; i++){
+				array.push(Math.floor(Math.random()*1000));
+			}
+			return array;
+		};
+		$scope.refreshData = function(){
+			$timeout(function(){
+				$scope.data0 = $scope.generateRandomArray(5);
+				$scope.data1 = $scope.generateRandomArray(3);
+
+				$scope.refreshData();
+			},2500);
+		};
+		$scope.refreshData();
+	}]);
+});
